e13ebf40b759cdbcf45a277e7a5bec0655611c93
szidorpatrik <patrikp987@gmail.com>
2023-10-06 17:18:49 +0200

    Added basic methods

--- GameOfLife/Entities/Interfaces/ICanBeEaten.cs
+++ GameOfLife/Entities/Interfaces/ICanBeEaten.cs
@@ -2,5 +2,9 @@ namespace GameOfLife.Entities.Interfaces;
 
 public interface ICanBeEaten
 {
+    //Returns true if the entity can be eaten
+    public bool CanBeEaten();
     
+    //Entity gets eaten and adds HP to the predator
+    public int GetEaten();
 }
