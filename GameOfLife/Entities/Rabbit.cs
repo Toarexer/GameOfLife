ca3c1c55b5e8a5495fa1a310aa4944a9426d35ea
szidorpatrik <patrikp987@gmail.com>
2023-10-06 17:22:55 +0200

    Implemented interfaces
    +ICanBeEaten
    +ICanAge

--- GameOfLife/Entities/Rabbit.cs
+++ GameOfLife/Entities/Rabbit.cs
@@ -3,8 +3,9 @@ using GameOfLife.Entities.Interfaces;
 
 namespace GameOfLife.Entities;
 
-public class Rabbit : Animal, ICanBeEaten
+public class Rabbit : Animal, ICanBeEaten, ICanMove, ICanBreed, ICanAge
 {
+    private int nutritionalValue = 3;
     public Rabbit()
     {
         Hp = 5;
@@ -19,5 +20,27 @@ public class Rabbit : Animal, ICanBeEaten
         Age = 0;
         this.posX = posX;
         this.posY = posY;
+    }
+
+    public bool CanBeEaten()
+    {
+        //If the predator is on current position, it can be eaten
+        return false;
+    }
+
+    public int GetEaten()
+    {
+        //Adds HP to the predator, then dies
+        return nutritionalValue;
+    }
+
+    public int GetAge()
+    {
+        return Age;
+    }
+
+    public void IncreaseAge(int unit)
+    {
+        Age += unit;
     }
 }
