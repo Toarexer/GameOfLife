778d0695544064497f7f394716ad475a19f83f23
szidorpatrik <patrikp987@gmail.com>
2023-10-06 17:23:07 +0200

    Added Die method

--- GameOfLife/Entities/Animal.cs
+++ GameOfLife/Entities/Animal.cs
@@ -6,3 +6,11 @@ namespace GameOfLife.Entities;
 public abstract class Animal : Entity, ICanMove, ICanBreed
 {
     public void Die()
+    {
+        if (Hp < 0)
+        {
+            //Destroys entity
+        }
+    }
+}
+
