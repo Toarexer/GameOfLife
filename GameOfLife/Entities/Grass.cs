dc806367a7ee3d18e242510527a2d85945d3cf18
szidorpatrik <patrikp987@gmail.com>
2023-10-06 16:56:29 +0200

    Basic functionality

--- GameOfLife/Entities/Grass.cs
+++ GameOfLife/Entities/Grass.cs
@@ -4,5 +4,34 @@ namespace GameOfLife.Entities;
 
 public class Grass : Entity, ICanSpread, ICanBeEaten
 {
+    public enum State
+    {
+        Seed = 0,
+        Tuft = 1,
+        Tender = 2
+    }
     
+    
+    public Grass()
+    {
+        Hp = (int)State.Seed;
+    }
+
+    public bool CanBeEaten()
+    {
+        //If it is in a state of Seed, it can not be eaten
+        return GetState() != State.Seed;
+    }
+
+    public int GetEaten()
+    {
+        //Returns 'nutritional' (Hp) value to the animal who eats it depending on the State of the seedling.
+        return (int)(State)Hp;
+    }
+
+    public State GetState()
+    {
+        //Returns the state of the seedling
+        return (State)Hp;
+    }
 }
