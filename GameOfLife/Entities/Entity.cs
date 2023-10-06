85c0751342d56d33e29fa15fe9dbc55a76aabb7c
szidorpatrik <patrikp987@gmail.com>
2023-10-06 17:18:11 +0200

    Changed visibility of Age

--- GameOfLife/Entities/Entity.cs
+++ GameOfLife/Entities/Entity.cs
@@ -5,7 +5,7 @@ namespace GameOfLife.Entities;
 public abstract class Entity
 {
     public int Hp { get; protected set; }
-    public int Age { get; protected set; }
+    protected int Age { get; set; }
     public Tuple<int> Position { get; protected set; }
     protected int posX { get; set; }
     protected int posY { get; set; }
