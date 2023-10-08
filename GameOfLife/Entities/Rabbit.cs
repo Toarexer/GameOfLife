2b0e526ce6249c8cd3d2153ffb13a06a45b0f877
szidorpatrik <patrikp987@gmail.com>
2023-10-08 17:33:05 +0200

    Implemented basic logic

--- GameOfLife/Entities/Rabbit.cs
+++ GameOfLife/Entities/Rabbit.cs
@@ -1,12 +1,15 @@
-using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using GameOfLife.Entities.Interfaces;
 using GameOfLife.GameLogic;
 
 namespace GameOfLife.Entities;
 
-public class Rabbit : Animal, ICanBeEaten, ICanMove, ICanBreed, ICanAge
+public class Rabbit : Animal, ICanBeEaten
 {
-    private int nutritionalValue = 3;
+    public readonly int NutritionalValue = 3;
+    private List<Simulable?> _neighbours = new List<Simulable?>(); 
     public Rabbit()
     {
         Hp = 5;
@@ -17,27 +20,107 @@ public class Rabbit : Animal, ICanBeEaten, ICanMove, ICanBreed, ICanAge
     {
         Hp = 5;
         Age = 0;
+        Position = (posX, posY);
+    }
+
+    public override void Update(Grid grid)
+    {
+        _neighbours = GetAllNeighbours(grid);
+        Move(grid);
+        Eat((Grass?)_neighbours.FirstOrDefault(x => x is Grass));
+        IncreaseAge(1);
+        Hp--;
+        Die();
+    }
+
+    public override bool ShouldCreateDescendant(Grid grid)
+    {
+        return false;
+    }
+
+    public override bool ShouldDie()
+    {
+        return Hp < 1 || CanBeEaten();
+    }
+
+    private List<Simulable?> GetAllNeighbours(Grid grid)
+    {
+        var entities = new List<Simulable?>();
+        
+        for (int i = -2; i < 3; i++)
+            for (int j = -2; j < 3; j++)
+            {
+                entities.Add(grid.SimsOfType<Grass>(Position.x + i, Position.y + j).FirstOrDefault());
+                entities.Add(grid.SimsOfType<Fox>(Position.x + i, Position.y + j).FirstOrDefault());
+                entities.Add(grid.SimsOfType<Rabbit>(Position.x + i, Position.y + j).FirstOrDefault());
+            }
+
+        return entities;
+    }
+
+    private bool CanMove()
+    {
+        return _neighbours.Count == 0 || !_neighbours.Any(x => x is Fox);
+    }
+
+    private void Move(Grid grid)
+    {
+        //Move randomly if hp is full
+        if (!ShouldEat() && CanMove())
+        {
+            MoveRandomly(grid);
+            return;
+        }
+
+        //Move to the next grass to eat else stays, preference by nutritional value
+        if (ShouldEat() && CanMove() && _neighbours.Any(x=> x is Grass))
+        {
+            Position = _neighbours
+                .Where(x => x is Grass)
+                .OrderByDescending(y => (Grass?)y)
+                .FirstOrDefault(x => x is Grass)?.Position ?? Position;
+        }
+    }
+
+    private void MoveRandomly(Grid grid)
+    {
+        var r = new Random();
+        int nextX; 
+        int nextY; 
+
+        do
+        {
+            nextX = Position.x + r.Next(-1, 2);
+            nextY = Position.y + r.Next(-1, 2);
+        }
+        while (!grid.WithinBounds(nextX, nextY));
+
+        Position = (nextX, nextY);
+    }
+    
+    private bool ShouldEat()
+    {
+        return Hp < 5;
+    }
+
+    public void Eat(Grass? grass)
+    {
+        if (ShouldEat() && grass != null)
+        {
+            Hp += grass.GetEaten();
+        }
     }
 
     public bool CanBeEaten()
     {
         //If the predator is on current position, it can be eaten
-        return false;
+        return _neighbours.Any(x=> x is Fox);
     }
 
-    public int GetEaten()
+    public void GetEaten()
     {
-        //Adds HP to the predator, then dies
-        return nutritionalValue;
-    }
-
-    public int GetAge()
-    {
-        return Age;
-    }
-
-    public void IncreaseAge(int unit)
-    {
-        Age += unit;
+        if (!CanBeEaten()) return;
+        var fox = (Fox)_neighbours.FirstOrDefault(x => x is Fox)!;
+        fox.Eat(this);
     }
 }
