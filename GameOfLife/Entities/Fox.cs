d9a78ed0aebddf8486b6964a7b11ecaef2a66afb
szidorpatrik <patrikp987@gmail.com>
2023-10-08 17:31:15 +0200

    Implemented basic eating

--- GameOfLife/Entities/Fox.cs
+++ GameOfLife/Entities/Fox.cs
@@ -1,3 +1,4 @@
+using GameOfLife.Entities.Interfaces;
 using GameOfLife.GameLogic;
 
 namespace GameOfLife.Entities;
@@ -6,13 +7,48 @@ public class Fox : Animal
 {
     public Fox()
     {
-        Hp = 5;
+        Hp = 10;
         Age = 0;
     }
 
     public Fox(int posX, int posY)
     {
-        Hp = 5;
+        Hp = 10;
         Age = 0;
     }
+
+    public bool ShouldEat()
+    {
+        return Hp < 7;
+    }
+
+    public void Eat(Rabbit rabbit)
+    {
+        if (!ShouldEat()) return;
+        Hp += rabbit.NutritionalValue;
+        rabbit.Die();
+    }
+    
+    public override void Update(Grid grid)
+    {
+        //Move();
+        //Eat();
+        
+
+        IncreaseAge(1);
+        Hp--;
+        Die();
+    }
+
+    public override bool ShouldCreateDescendant(Grid grid)
+    {
+        return base.ShouldCreateDescendant(grid);
+    }
+
+    public override bool ShouldDie()
+    {
+        return Hp < 1;
+    }
+    
+    
 }
