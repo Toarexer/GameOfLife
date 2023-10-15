cbcc02eae5bf847eff1c113bb2e6b114a7d8696d
szidorpatrik <patrikp987@gmail.com>
2023-10-15 17:03:56 +0200

    Better readability

--- GameOfLife/Entities/MatingPair.cs
+++ GameOfLife/Entities/MatingPair.cs
@@ -2,17 +2,25 @@ using GameOfLifeSim;
 
 namespace GameOfLife.Entities
 {
-    /// <summary>A class to form mating pairs for T animals.</summary>
+    /// <summary>
+    /// A class to form mating pairs for T animals.
+    /// </summary>
     /// <typeparam name="T">The type of objects that can form mating pairs.</typeparam>
     public class MatingPair<T> where T : ISimulable
     {
-        /// <summary>The first member of the mating pair.</summary>
+        /// <summary>
+        /// The first member of the mating pair.
+        /// </summary>
         public T MatingPair1 { get; init; }
 
-        /// <summary>The second member of the mating pair.</summary>
+        /// <summary>
+        /// The second member of the mating pair.
+        /// </summary>
         public T MatingPair2 { get; init; }
 
-        /// <summary>Initializes a new instance of the MatingPair class with the provided mating pair members.</summary>
+        /// <summary>
+        /// Initializes a new instance of the MatingPair class with the provided mating pair members.
+        /// </summary>
         /// <param name="matingPair1">The first member of the mating pair.</param>
         /// <param name="matingPair2">The second member of the mating pair.</param>
         public MatingPair(T matingPair1, T matingPair2)
