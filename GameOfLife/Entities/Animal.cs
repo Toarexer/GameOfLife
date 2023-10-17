d30b4b1af56ea24e126498a8ae23e4629de67201
szidorpatrik <patrikp987@gmail.com>
2023-10-17 21:19:01 +0200

    Added invincibility window and mating cooldown

--- GameOfLife/Entities/Animal.cs
+++ GameOfLife/Entities/Animal.cs
@@ -8,12 +8,27 @@ namespace GameOfLife.Entities
     /// </summary>
     public abstract class Animal : ICanMove, ICanBreed, ICanAge
     {
+        private int _invincibility;
+        private int _matingCooldown;
+
         /// <summary>HP (Hit points) will determine if the animal needs to eat or should die.</summary>
         public int Hp { get; protected set; }
 
         /// <summary>Age determines if the animal can breed or can be eaten.</summary>
         protected int Age { get; set; }
 
+        protected int Invincibility
+        {
+            get => _invincibility < 0 ? 0 : _invincibility;
+            set => _invincibility = value;
+        }
+
+        protected int MatingCooldown
+        {
+            get => _matingCooldown < 0 ? 0 : _matingCooldown;
+            set => _matingCooldown = value;
+        }
+        
         /// <summary>Returns the Age of the animal.</summary>
         /// <returns>int Age of the animal</returns>
         public int GetAge()
@@ -26,6 +41,9 @@ namespace GameOfLife.Entities
         public void IncreaseAge(int unit)
         {
             Age += unit;
+            //Hp--;
+            Invincibility--;
+            MatingCooldown--;
         }
     }
 }
