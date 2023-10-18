bc2003dc6f46081580be9ff9b2bd4f3d6329dd3c
szidorpatrik <patrikp987@gmail.com>
2023-10-18 17:29:23 +0200

    implemented new logic and logging

--- GameOfLife/Entities/Fox.cs
+++ GameOfLife/Entities/Fox.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using GameOfLifeLogger;
 using GameOfLifeSim;
 
 namespace GameOfLife.Entities;
@@ -8,7 +9,10 @@ namespace GameOfLife.Entities;
 /// <summary>
 /// Represents a fox, a type of animal in the simulation.
 /// </summary>
-public class Fox : Animal, ISimulable {
+public class Fox : Animal, ISimulable, IComparable<Fox>
+{
+    private const int MaxHp = 8;
+    private MatingPair<Fox>? _matingPair;
     private List<Rabbit> _rabbits = new();
     private List<Fox> _foxes = new();
     
@@ -17,7 +21,7 @@ public class Fox : Animal, ISimulable {
     /// </summary>
     /// <param name="position">The initial position of the fox.</param>
     public Fox(GridPosition position) {
-        Hp = 10;
+        Hp = MaxHp;
         Age = 0;
         Position = position;
         Invincibility = 3;
@@ -25,57 +29,89 @@ public class Fox : Animal, ISimulable {
     }
 
     public override void Update(Grid grid) {
-        _rabbits = new List<Rabbit>(grid.SimsOfTypeInRadius<Rabbit>(Position.X, Position.Y, 2));
-        _foxes = new List<Fox>(grid.SimsOfTypeInRadius<Fox>(Position.X, Position.Y, 2));
+        _rabbits = grid.SimsOfTypeInRadius<Rabbit>(Position.X, Position.Y, 2).OrderByDescending(x => x).ToList();
+        _foxes = grid.SimsOfTypeInRadius<Fox>(Position.X, Position.Y, 2).Except(this).OrderByDescending(x => x).ToList();
 
         Move(grid);
-        if (_rabbits.Count > 0) Eat(_rabbits.First());
+        Eat();
         IncreaseAge(1);
-        Hp--;
+        Logger.Info(this.ToString());
+    }
+    
+    protected override bool ShouldEat() 
+    {
+        return Hp < MaxHp;
+    }
+    
+    protected override void Eat() {
+        if (_rabbits.Count < 1 || HasMatingPartner || !ShouldEat()) return;
+        Random rnd = new();
+            
+        Rabbit[] optimalRabbits = _rabbits.Where(x => x.CanBeEaten() && x.NutritionalValue + this.Hp == MaxHp).ToArray();
+        if (optimalRabbits.Length < 1) return;
+        
+        Rabbit rabbitToEat = optimalRabbits[rnd.Next(0, optimalRabbits.Length)];
+        
+        NextPosition = rabbitToEat.Position;
+        Hp += rabbitToEat.GetEaten();
     }
 
-    /// <summary>
-    /// Attempts to eat a rabbit if conditions allow.
-    /// </summary>
-    /// <param name="rabbit">The rabbit to eat.</param>
-    /// <returns>True if the fox successfully eats the rabbit; otherwise, false.</returns>
-    public void Eat(Rabbit rabbit) {
-        if (!ShouldEat()) return;
-
-        int rabbitHpToGive = rabbit.GetEaten();
-        if (rabbitHpToGive == 0) return;
-
-        Hp += rabbitHpToGive;
-        NextPosition = rabbit.Position;
+    public override bool ShouldDie() 
+    {
+        if (Hp < 1)
+        {
+            Logger.Info($"Should Die: {this}");
+            return true;
+        }
+        return false;
     }
 
+    protected override bool ShouldCreateDescendant() 
+    {
+        if (_matingPair != null && _foxes.Count < 1 && MatingCooldown > 0) return HasMatingPartner;
+        
+        var matingPairFox = _foxes.Order().FirstOrDefault();
 
-    protected override bool ShouldEat() {
-        return Hp < 8;
-    }
+        if (matingPairFox == null) return false;
+        _matingPair = new MatingPair<Fox>(this, matingPairFox)
+        {
+            MatingPair1 = { HasMatingPartner = true }
+        };
 
-    public override bool ShouldDie() {
-        return Hp < 1;
-    }
-
-    protected override bool ShouldCreateDescendant() {
-        return _foxes.Any();
+        Logger.Info(_matingPair.ToString());
+        return HasMatingPartner && _foxes.Count == 1;
     }
 
     public override ISimulable? NewDescendant(Grid grid) {
-        if (ShouldCreateDescendant())
-            return new Fox(Position);
-        return null;
+        if (!ShouldCreateDescendant()) return null;
+        
+        if (_matingPair?.MatingPair1 != null)
+        {
+            _matingPair.MatingPair1.HasMatingPartner = false;
+        }
+        
+        if (_matingPair?.MatingPair2 != null)
+        {
+            _matingPair.MatingPair2.HasMatingPartner = false;
+            _matingPair.MatingPair2.MatingCooldown = 2;
+        }
+
+        _matingPair = null;
+        MatingCooldown = 3;
+        return new Fox(Position);
     }
 
     protected override void Move(Grid grid) {
-        if (!ShouldEat()) {
+        if (!ShouldEat()) 
+        {
             MoveRandomly(grid);
             return;
         }
-
-        if (ShouldEat() && _rabbits.Any()) {
-            NextPosition = _rabbits.First().Position;
+        
+        if (HasMatingPartner && _matingPair != null)
+        {
+            MoveRandomly(grid);
+            _matingPair!.MatingPair2.NextPosition = Position;
         }
     }
     
@@ -84,4 +120,14 @@ public class Fox : Animal, ISimulable {
     /// </summary>
     /// <returns>A `DisplayInfo` object containing type information and a color (orange).</returns>
     public DisplayInfo Info() => new(GetType().FullName ?? GetType().Name, new(255, 127, 0));
+
+    public int CompareTo(Fox? other)
+    {
+        return Age.CompareTo(other?.Age);
+    }
+    
+    public override string ToString()
+    {
+        return $"Fox:\t {Position}\t|\tHp: {Hp}\t|\tInvincibility: {Invincibility}\t|\tMating CD: {MatingCooldown}  \t|\tHasPair? {HasMatingPartner}\t|";
+    }
 }
